06d8ce8457e716cfc86d88e30b4321a32a9ca0a8
Gustavo Alfonso Cuervo Moreno <gustavoalfonsocuervomoreno@MacBook-Pro-de-Gustavo.local>
2021-09-13 16:34:00 -0500

    Tarea explicación niño 12 años

---
+++ Desarrollo-homework-02-JS-I.js
@@ -0,0 +1,71 @@
+Desarrollo homework 02-JS-I
+* Variables
+	Son una cantidad de cajoncitos donde guardas algo. A cada cajón le das un nombre para no confundirlo con otro. En cada cajón guardas un dato. Para bautizar cada cajón debes poner, siempre, "var"
+		var nombre1="Gustavo"
+		var nombre2="A."
+		var apellidopaterno="Cuervo"
+		var apellidomaterno="M."
+		var tuedad=70
+
+* Strings
+	Son cajones donde guardas datos formados por las letras del alfabeto y otos caracteres como por ejemplo, en un cajón guardas tu primer nombre y en otro cajón guardas el nombre de tú gato. Ambos formados por la combinación de vocales y consonantes del alfabeto.
+
+* Funciones (argumentos, `return`)
+
+	Una función son las instrucciones que se dan para desarrollar una tarea. Debes bautizarla.
+
+		Bañarse
+		Hacer arroz
+		Tender la cama
+		amarrarse los zapatos
+		hacer las tareas
+
+	Para que sepas que es una función debes poner siempre "funtion"
+
+		function bañarse (){}
+		function hacerarroz (){}
+		function Tenderlacama (){}
+		function amarrarseloszapatos (){}
+		function hacerlastareas (){}
+
+	Vamos a mirar los argumento para la funcion Hacer arroz
+
+		Necesitas paila, cuchillo, aceite, agua, sal, arroz, cebolla, ajo, cuchillos, posillo, cuantas tasas vas a preparar (1)
+
+		- picar la cebolla
+		- en la paila echar: 
+			1. la medida de aceite
+			2. la cebolla picada
+			3. la porción de ajo
+			4. la porción de sal
+			5. poner a sofreir en la estufa
+			6. esta sofreido?
+			6. ehar dos tasas de agua 
+			7. dejar hervir
+			8. echar una tasa de arroz
+			9. se evaporo el agua?
+			10. bajar la llama
+			12. está seco
+			13 apagar
+			14 servir
+		
+	function hacerarroz(paila, cuchillo, aceite, agua, sal, arroz, cebolla, ajo, cuchillos, posillo, cantidad=1)
+	{return
+	}
+
+* Declaraciones `if`
+	
+	'if'  cuando una condisión se cumple hace una tarea determinada.  cuando la condidión no se cumple continua haciendo otra tarea 
+
+		por ejemplo si estas seleccionando unos zapatos te preguntas si te gustan los que tomaste si te gustan te los pones. Si no te gustan buscas otros.
+
+* Valores booleanos (`true`, `false`)
+
+	Una variable booliana tiene basicamente dos alternativas o se cumple 'true' o no se cumple 'false'
+	
+
+
+
+
+
+	
