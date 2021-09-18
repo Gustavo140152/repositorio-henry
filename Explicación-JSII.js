35eff40332fac5d3f833b654dd87b62be4cb8a53
Gustavo Alfonso Cuervo Moreno <gustavoalfonsocuervomoreno@MacBook-Pro-de-Gustavo.local>
2021-09-18 13:52:02 -0500

    ?explicació

---
+++ Explicación-JSII.js
@@ -0,0 +1,11 @@
+Explicación JSII
+* `for`
+Inicialmente tienes un cajón vacio. Haces una tarea como por ejemplo un ejercicio de Gym consistente en 10 flexiones. Cuendo terminas el ejercicio hechas una bolita en el cajón. esto lo vas a hacer durante 10 días. al segundo dìa miras si en el cajon hay 10 bolitas como no las hay hacer las 10 flexiones y hechas ota bolita en el cajón. Haces esto todos los dìa hasta completa la 10 bolitas en el cajón y paras de hacer los ejercios. Como resultado te sientes mejor y etas muy contento porque cumpliste con lo que te propusiste.
+
+* `&&`, `||`, `!`
+
+	'&&' para que el computado entien que es "y" y consiste en que para que puedas salir a jugar con tus amigo debes Hacer las Tareas ´y` arreglar el cuart si no has hecho las dos cosas no puedes salir a jujar
+
+	'||' lo mismo para que el computador entienda significa "ó" para poder salir a la calle  debes haber terminado "ò" Haber sacado buenas notas en las tareas del día anterior
+
+	'!` tú sabes que una cosa no es cierta como por ejemplo haber termido la tarea pero aún no es así y para poder salir a jugar le pides a tu hermana que diga que ya la hiciste.
