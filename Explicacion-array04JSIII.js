df1036ebe17ee1ba6c0a2c54259287c5d5c40e4a
Gustavo Alfonso Cuervo Moreno <gustavoalfonsocuervomoreno@MacBook-Pro-de-Gustavo.local>
2021-09-19 10:53:36 -0500

    Explicación array

---
+++ Explicacion-array04JSIII.js
@@ -0,0 +1,5 @@
+Explicacion_array
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+	* Arrays
+	Un array es como tu closet un sitio con muchos cajones donde en cada cajón guardas algo. camisas, pantalones, zapatos .....
